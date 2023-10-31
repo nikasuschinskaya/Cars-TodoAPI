90f5d468823c423ba8cb3a5b508c1fdc9403c2b1
nikasuschinskaya@gmail.com <nikasuschinskaya@gmail.com>
2023-10-31 13:44:23 +0300

    delete useless classes

--- Cars/Cars.WebAPI/Program.cs
+++ Cars/Cars.WebAPI/Program.cs
@@ -8,24 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-
-
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddScoped<IRepository<CarEntity>, CarRepository>();
 
 var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
 
 if (builder.Environment.IsDevelopment())
 {
